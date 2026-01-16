bb7a64d69bfdab672da246253fb52ba9e12c84c3
hai bui <hoanghaibui2003@gmail.com>
2026-01-16 10:13:38 +0700

    update js files

---
+++ neu.js
@@ -0,0 +1,12 @@
+let isOnline = true;
+let isPaid = false;
+
+if (isOnline === true) {
+    if (isPaid === true) {
+        console.log("Cho vao");
+    } else {
+        console.log("Chua tra tien");
+    }
+} else {
+    console.log("Offline");
+}
