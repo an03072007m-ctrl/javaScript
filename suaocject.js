c86e46ed5dc0ceef2f5e3fa0fdbf8372ba77b82a
hai bui <hoanghaibui2003@gmail.com>
2026-01-24 16:33:53 +0700

    ngay 9

---
+++ suaocject.js
@@ -0,0 +1,13 @@
+let student = {
+    name: "Pon",
+    age: 18,
+    isPassed: true
+};
+student.age = 19
+student.score = 100
+
+
+
+console.log(`Name:${student.name} 
+Score: ${student.score}
+Age: ${student.age}`);
