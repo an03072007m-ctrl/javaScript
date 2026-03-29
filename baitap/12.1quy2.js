b0e73feddeec2381edbec9ac053ea0156ea07e9c
hai bui <hoanghaibui2003@gmail.com>
2026-03-29 14:41:47 +0700

    12.1quy2

--- baitap/12.1quy2.js
+++ baitap/12.1quy2.js
@@ -14,9 +14,24 @@ function getAverageScore(){
 
     return total / students.length;
 }
+
+function getGoodStudents(){
+    return students.filter(s=>s.score >=8)
+
+
+}
+function getTopStudents(){
+    if(students.length ===0)return null
+    let sorted =[...students].sort((a,b)=> b.score - a.score);
+    return [sorted[0]|| null];
+
+
+}
 addStudent(1, "Hai", 8);
 addStudent(2, "Nam", 6);
 addStudent(3, "Linh", 9);
 console.log(findStudent("Nam"));
 
 console.log(getAverageScore());
+console.log(getGoodStudents());
+console.log(getTopStudents());
