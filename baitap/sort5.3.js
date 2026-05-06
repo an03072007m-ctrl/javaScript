df29897788a5a2171ca5bb01383a70a1db938f54
hai bui <hoanghaibui2003@gmail.com>
2026-05-06 15:16:07 +0700

    sort5.3

--- baitap/sort5.3.js
+++ baitap/sort5.3.js
@@ -1,3 +1,32 @@
-function notsort(){
-
+function quicksleep(arr,k){
+    if ( arr.length ===1)return arr [0]
+    let pivot = arr[0]
+    let L = []
+    let R = []
+    for(let i = 1; i < arr.length; i++){
+        if( arr[i] < pivot){
+             L.push(arr[i])
+        }else{
+            R.push(arr[i])
+        }
+    }
+    if (k< L.length){
+        return quicksleep(L,k)
+    }
+    if (k === L.length){
+        return pivot
+    }
+    return quicksleep(R,k - L.length - 1)
 }
+function topKSmallest(arr,k){
+    const kth = quicksleep(arr,k -1);
+    let result = []
+    for (let num of arr){
+        if (num <= kth){
+            result.push(num);
+        }
+
+    }
+    return result
+}
+console.log (topKSmallest([7, 10, 4, 3, 20, 15],3))
